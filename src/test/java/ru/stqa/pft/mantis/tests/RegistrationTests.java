9a8ef876d396939bab44cd9c7b786305f935fd5a
max <rodionovmax90test@gmail.com>
2019-09-17 13:49:49 -0400

    Lesson 8.7 completed

--- src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -21,11 +21,13 @@ public class RegistrationTests extends TestBase{
     @Test
     public void testRegistration() throws IOException, MessagingException {
         long now = System.currentTimeMillis();
-        String email = String.format("user%s@localhost.localdomain", now);
         String user = String.format("user%s", now);
         String password = "password";
+        String email = String.format("user%s@localhost.localdomain", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+//        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         Assert.assertTrue(app.newSession().login(user, password));
@@ -37,7 +39,7 @@ public class RegistrationTests extends TestBase{
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+//    @AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
